0c4e074746590e0a4e57708ff29ec636b6084e6c
Razvan Rujoiu <rjrazvan@icloud.com>
2021-05-07 20:52:42 +0300

    fixed list view

--- app/src/main/java/com/example/gitrepositoryandroid/MainActivity.java
+++ app/src/main/java/com/example/gitrepositoryandroid/MainActivity.java
@@ -1,30 +1,22 @@
 package com.example.gitrepositoryandroid;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
-import android.app.Activity;
-import android.view.Menu;
+
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    String s[]={"cal", "magar", "iepure"};
-    String [] listItem;
+    String[] s=new String[]{"cal", "magar", "iepure"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter adapter=new ArrayAdapter<String>(this, R.layout.activity_main,s);
-        ListView listView=(ListView)findViewById(R.id.list1);
+        ArrayAdapter adapter= new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, s);
+        ListView listView=(ListView)findViewById(R.id.listView);
         listView.setAdapter(adapter);
-
-        TextView helloTextView= (TextView) findViewById(R.id.textView);
-        helloTextView.setText(Arrays.toString(s));
     }
 
 }
