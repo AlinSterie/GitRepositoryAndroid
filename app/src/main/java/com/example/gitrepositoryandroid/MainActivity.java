4c42f51942464c6470d507098f36d4a06cfbc9ec
AlinSterie <57179317+AlinSterie@users.noreply.github.com>
2021-05-07 20:06:40 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/gitrepositoryandroid/MainActivity.java
+++ app/src/main/java/com/example/gitrepositoryandroid/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.gitrepositoryandroid;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.app.Activity;
+import android.view.Menu;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    String s[]={"cal", "magar", "iepure"};
+    String [] listItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter adapter=new ArrayAdapter<String>(this, R.layout.activity_main,s);
+        ListView listView=(ListView)findViewById(R.id.list1);
+        listView.setAdapter(adapter);
+
+        TextView helloTextView= (TextView) findViewById(R.id.textView);
+        helloTextView.setText(Arrays.toString(s));
     }
+
 }
