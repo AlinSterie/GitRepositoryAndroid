27d0346d47ea3714a9462f2e36525dac49f44865
DESKTOP-BNNE5HK\alins <alinsterie@ymail.com>
2021-05-13 00:51:24 +0300

    GET request from url

--- app/src/main/java/com/example/gitrepositoryandroid/MainActivity.java
+++ app/src/main/java/com/example/gitrepositoryandroid/MainActivity.java
@@ -2,8 +2,23 @@ package com.example.gitrepositoryandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -14,18 +29,52 @@ public class MainActivity extends AppCompatActivity {
     Animal horse=new Animal("Marcel","10","Arabian","500");
     private ArrayList<Animal> arrayList=new ArrayList<>();
     MyAdapter adapter;
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView=findViewById(R.id.listView);
-        arrayList.add(dog);
-        arrayList.add(cat);
-        arrayList.add(horse);
-        adapter=new MyAdapter(this,arrayList);
-        listView.setAdapter(adapter);
-
+        mTextViewResult=findViewById(R.id.text_view_result);
+        Button buttonParse=findViewById(R.id.button_parse);
+        mQueue= Volley.newRequestQueue(this);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jasonParse();
+            }
+        });
 
     }
+    private void jasonParse(){
+        String url="https://run.mocky.io/v3/9bb8aac5-2cce-4b35-ae8a-6f99939d8584";
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, url, null
+                , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray=response.getJSONArray("animals");
+                    mTextViewResult.append("\n\n\n");
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject animals=jsonArray.getJSONObject(i);
+                        String name=animals.getString("name");
+                        String age=animals.getString("age");
+                        String race= animals.getString("race");
+                        String weight=animals.getString("weight");
+                        mTextViewResult.append(name+", "+age+", "+race+", "+weight+"\n\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
 
 }
